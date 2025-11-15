e2e2c96507e9ed79ab2250d4bfcc5b2915afc287
Abdul Vajid <vvajidz56@gmail.com>
2025-11-16 00:25:36 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// Pure SPA config, no Express server injected
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Vercel will serve this folder
     outDir: "dist/spa",
   },
   resolve: {
@@ -15,9 +13,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./client"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
-  },
-  server: {
-    host: "::",
-    port: 8080,
   },
 });
